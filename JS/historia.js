5a292b13cf69809aeb608dac56b827317936a5bf
Anton0212 <c3pian00@students.oamk.fi>
2024-05-05 10:11:46 +0300

    hisotoria peli

--- JS/historia.js
+++ JS/historia.js
@@ -3,23 +3,34 @@ document.addEventListener('DOMContentLoaded', function() {
     var nextButton = document.getElementById('next-button');
     nextButton.addEventListener('click', nextQuestion);
 
-var questions = [
-    {
-        question: "Kuka oli Suomen kuningas keskiajalla?",
-        options: ["Kustaa Vaasa", "Eerik Pommerilainen", "Kaarle Suuri", "Maunu Eerikinpoika"],
-        correctAnswer: "Eerik Pommerilainen"
-    },
-    {
-        question: "Mikä oli yleisin ammatti keskiajalla?",
-        options: ["Ritari", "Kauppias", "Maanviljelijä", "Kirkonmies"],
-        correctAnswer: "Maanviljelijä"
-    },
-    {
-        question: "Missä uskottiin keskiajalla järjestettävän ritariturnajaisia?",
-        options: ["Kööpenhaminassa", "Lontoossa", "Pariisissa", "Nürnbergissä"],
-        correctAnswer: "Pariisissa"
-    }
-];
+    var questions = [
+        {
+            question: "Kuka oli Suomen kuningas keskiajalla?",
+            options: ["Kustaa Vaasa", "Eerik Pommerilainen", "Kaarle Suuri", "Maunu Eerikinpoika"],
+            correctAnswer: "Eerik Pommerilainen"
+        },
+        {
+            question: "Mikä oli yleisin ammatti keskiajalla?",
+            options: ["Ritari", "Kauppias", "Maanviljelijä", "Kirkonmies"],
+            correctAnswer: "Maanviljelijä"
+        },
+        {
+            question: "Missä uskottiin keskiajalla järjestettävän ritariturnajaisia?",
+            options: ["Kööpenhaminassa", "Lontoossa", "Pariisissa", "Nürnbergissä"],
+            correctAnswer: "Pariisissa"
+        },
+        {
+            question: "Mikä oli keskiajan yleisin juoma?",
+            options: ["Viini", "Olut", "Vesi", "Maito"],
+            correctAnswer: "Olut"
+        },
+        {
+            question: "Kuka oli kuuluisa englantilainen keskiajan kirjailija?",
+            options: ["William Shakespeare", "Geoffrey Chaucer", "Jane Austen", "Charles Dickens"],
+            correctAnswer: "Geoffrey Chaucer"
+        }
+    ];
+    
 
 var currentQuestionIndex = 0;
 
@@ -62,12 +73,22 @@ function nextQuestion() {
         displayQuestion();
         document.getElementById('result').textContent = '';
     } else {
-       
         var questionContainer = document.getElementById('question-container');
         questionContainer.innerHTML = '<p>Kaikki kysymykset suoritettu!</p>';
         document.getElementById('result').textContent = '';
+
+       
+        var restartButton = document.createElement('button');
+        restartButton.textContent = 'Aloita uusi peli';
+        restartButton.onclick = function() {
+            currentQuestionIndex = 0; 
+            displayQuestion(); 
+        };
+        questionContainer.appendChild(restartButton);
     }
 }
+
+
 
 
 displayQuestion();
